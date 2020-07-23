a80f20ac76a53313e658e591ce73f86bfec4ba04
Purnima G Katam (S-59) <68459602+pgkatam997@users.noreply.github.com>
2020-07-23 21:03:37 +0530

    A

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+var board=[10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180];
+
+const player1_Button = document.getElementById('player-1');
+const player2_Button = document.getElementById('player-2');
+
+console.log(player1_Button);
